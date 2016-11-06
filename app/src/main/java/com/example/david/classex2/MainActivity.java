1526032aefeb1b6e8853906846e31061dd483c09
David Ohayon <DavidOhayonSe@gmail.com>
2016-11-06 11:20:20 +0200

    arrange view

--- app/src/main/java/com/example/david/classex2/MainActivity.java
+++ app/src/main/java/com/example/david/classex2/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.david.classex2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch s1 ;
+    TextView t1;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        s1 = (Switch) findViewById(R.id.switch1);
+        t1 = (TextView) findViewById(R.id.l1);
+        b1 = (Button) findViewById(R.id.button);
+
     }
 }
